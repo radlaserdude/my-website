33b9affa1b6de381620fbf90bf776cb5d3061484
radlaserdude <radlaserdude@gmail.com>
2026-04-25 22:26:32 -0400

    Update fetch-reading.js

--- scripts/fetch-reading.js
+++ scripts/fetch-reading.js
@@ -1,39 +1,17 @@
-const fs = require("fs");
-const https = require("https");
+<script>
+  async function loadReading() {
+    try {
+      const res = await fetch('reading.json');
+      const data = await res.json();
 
-function fetchPage(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let data = "";
+      document.body.innerHTML += "<p>Loaded OK</p>";
+      document.body.innerHTML += "<p>" + data.title + "</p>";
 
-      res.on("data", chunk => data += chunk);
-      res.on("end", () => resolve(data));
-    }).on("error", reject);
-  });
-}
-
-async function run() {
-  try {
-    const html = await fetchPage("https://www.jftna.org/jft/");
-
-    const titleMatch = html.match(/<h1.*?>(.*?)<\/h1>/i);
-    const textMatch = html.match(/<p>(.*?)<\/p>/i);
-
-    const title = titleMatch ? titleMatch[1].replace(/<.*?>/g, "") : "Just For Today";
-    const text = textMatch ? textMatch[1].replace(/<.*?>/g, "") : "Stay positive.";
-
-    const data = {
-      title,
-      text,
-      link: "https://www.jftna.org/jft/"
-    };
-
-    fs.writeFileSync("reading.json", JSON.stringify(data, null, 2));
-    console.log("Reading updated");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+    } catch (err) {
+      document.body.innerHTML += "<p style='color:red;'>ERROR LOADING JSON</p>";
+      console.error(err);
+    }
   }
-}
 
-run();
+  loadReading();
+</script>
